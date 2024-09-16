f09de94c10e59c5c7ffb94799fd58a908c1f90f9
Marcellous11 <1mcj001@gmail.com>
2024-09-16 16:03:29 -0500

    rebased

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import openai
 print("This is the local repo")
 print("This is the local repo")
 
-newData = ["the","cat","feel"]
+newData = ["the", "cat", "feel"]
+newData = [100, 200, 300]
 
 for cal in newData:
     print(cal)
