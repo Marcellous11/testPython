c45758029f1899cb69fc50a7a52ee0342cf947cb
Marcellous11 <1mcj001@gmail.com>
2024-09-16 15:59:28 -0500

    all good

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import openai
 
 
+print("This is the local repo")
+print("This is the local repo")
+
+newData = [1, 2, 34, 34, 5, 6, 7, 8, 9, 10]
+
+for cal in newData:
+    print(cal)
 print("This is my new change I am pushing up")
